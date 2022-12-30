69eb729cb5d08b7c609f1f1a9ea3b6fccef3708c
PrasannaP <mail2lulu@gmail.com>
2022-12-30 09:34:43 +0530

    ground

--- src/App.js
+++ src/App.js
@@ -13,21 +13,26 @@ console.log("pkp:  ~ file: App.js:6 ~ Scene")
 
 export default function App() {
   return (
-    <Suspense fallback={null}>
-      <Canvas shadows flat linear>
-        <directionalLight intensity={0.7} position={[20, 30, 30]} />
-        <OrthographicCamera makeDefault far={10000} near={0.1} zoom={70} position={[0, 100, 1000]} />
-        <hemisphereLight intensity={0.75} color="#eaeaea" position={[0, 1, 0]} />
 
-        <Scene />
-        <Physics iterations={6} gravity={[0, -0.1, 0]}>
-          <Debug scale={1.1}>
-            <Little position={[2.5, 1, 0]} rotation={[0, -0.5, 0]} />
-          </Debug>
-        </Physics>
-        <OrbitControls />
-      </Canvas>
-    </Suspense>
+    <Canvas shadows flat linear>
+      <color attach="background" args={['#feeaea']} />
+      <directionalLight intensity={0.7} position={[20, 30, 30]} />
+      <OrthographicCamera makeDefault far={10000} near={0.1} zoom={1} position={[0, 100, 1000]} />
+      <hemisphereLight intensity={0.75} color="#eaeaea" position={[0, 1, 0]} />
+
+      <Scene />
+      <Physics iterations={6} gravity={[0, -20, 0]}>
+        <Debug scale={1.1}>
+
+          <Little position={[125, 170, 0]} rotation={[-0.2, .2, .4]} />
+
+          <Ground rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} />
+
+        </Debug>
+      </Physics>
+      <OrbitControls />
+    </Canvas>
+
   )
 }
 
@@ -46,5 +51,10 @@ function Little(props) {
       { type: 'Sphere', args: [0.1], position: [0.2, -0.9, 0.5] },
     ],
   }))
-  return <Clone ref={little} scale={0.01} position={[0, 0, 0]} object={nodes.Sphere} dispose={null} />
+  return <Clone ref={little} scale={1} position={[0, 0, 0]} object={nodes.Sphere} dispose={null} />
+}
+
+function Ground(props) {
+  usePlane(() => ({ type: 'Static', ...props }))
+  return <ContactShadows position={[0, -3, 0]} scale={200} blur={2} far={3} opacity={1} />
 }
